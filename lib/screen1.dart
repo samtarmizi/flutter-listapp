f033b737a9dd9ebf5a29ec7f7f83662ca5df2f46
MUHAMMAD TARMIZI SANUSI AFKAR <realtarmizisanusi@gmail.com>
2020-01-18 15:25:41 +0800

    Passing data on other page

--- lib/screen1.dart
+++ lib/screen1.dart
@@ -1,25 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:list_app/screen2.dart';
-  
-class Screen1 extends StatelessWidget {
+
+class Screen1 extends StatefulWidget {
+  @override
+  _Screen1State createState() => _Screen1State();
+}
+
+class _Screen1State extends State<Screen1> {
+  final controller1 = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Screen 1'),
-        ),
-        body: Center(
-          child: Container(
-            child: RaisedButton(
+      appBar: AppBar(
+        title: Text('Screen 1'),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            TextField( controller: controller1,),
+            RaisedButton(
               child: Text('Screen 1'),
-              onPressed: (){
-                Navigator.push(context, MaterialPageRoute(
-                  builder: (context) => Screen2()
-                ));
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => Screen2(objectPassed: controller1.text)));
+                print(controller1.text);
               },
             ),
-          ),
+          ],
         ),
-      );
+      ),
+    );
   }
 }
--- lib/screen2.dart
+++ lib/screen2.dart
@@ -1,27 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:list_app/screen3.dart';
-  
-class Screen2 extends StatelessWidget {
+
+class Screen2 extends StatefulWidget {
+  final String objectPassed;
+  Screen2({Key key, this.objectPassed}): super(key: key);
+
+  @override
+  _Screen2State createState() => _Screen2State();
+}
+
+class _Screen2State extends State<Screen2> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Screen 2'),
-        ),
-        body: Center(
-          child: Container(
-            child: RaisedButton(
+      appBar: AppBar(
+        title: Text('Screen 2'),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Text('${widget.objectPassed}'),
+            RaisedButton(
               child: Text('Screen 3'),
-              onPressed: (){
+              onPressed: () {
                 // Navigator.push(context, MaterialPageRoute(
                 //   builder: (context) => Screen3()
                 // ));
                 Navigator.pushNamed(context, '/screen3');
               },
             ),
-          ),
+          ],
         ),
-  
+      ),
     );
   }
 }
